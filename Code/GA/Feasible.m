74c464a64368fb7df82993ab9f70a5fe0e4df00a
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-14 00:00:00 +0330

    Feasible.m modified

---
+++ Code/GA/Feasible.m
@@ -0,0 +1,38 @@
+function [ feasible ] = Feasible( x, courseTable, teacherSlotTable, nCourses)
+
+    feasible = true;
+% H1: all courses should be covered
+
+        [counts, courses] = hist(x, unique(x));
+        
+        [courses, index] = sort (courses);
+        
+        counts = counts(index);
+        
+        counts = counts(1,2:end);
+        
+        if (~isequal(courseTable', counts))
+            
+            feasible = false;
+            return
+
+        end
+        
+% H2: teachers should be at uni
+        for i=1:nCourses
+            
+           
+            t = x.*teacherSlotTable(i,2:end);
+            
+            p = find(t==i);
+            
+            if isempty(p)
+                feasible = false;
+                return
+                
+            end
+            
+            
+        end
+
+
