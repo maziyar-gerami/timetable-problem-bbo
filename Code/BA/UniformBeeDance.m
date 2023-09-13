98258ae1417d2cc4ef881e3a086ed54b3c7df0d3
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-14 00:00:00 +0330

    UniformBeeDance.m modified

---
+++ Code/BA/UniformBeeDance.m
@@ -0,0 +1,22 @@
+function [ y ] = UniformBeeDance( x,courseTable, teacherSlotTable, nCourses )
+
+y=x;
+feasible = false;
+
+counter=1;
+while(~feasible&& counter<100)
+    
+    first = randi(length(x)-1, 1,1);
+    second = first+1;
+
+    y(second) = x(first);
+    y(first)=x(second);
+    feasible = Feasible ( y, courseTable, teacherSlotTable, nCourses);
+    counter=counter+1;
+end
+
+
+
+
+end
+
